dddca7e4492099d511fa10eb4a00f8be527bb3cd
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-03-27 17:58:05 +0430

    Create OrderItem model

---
+++ models/OrderItem.js
@@ -0,0 +1,25 @@
+const { Model, UUID, UUIDV4, TEXT, SMALLINT } = require("sequelize");
+const sequelize = require("./sequelize");
+
+class OrderItem extends Model {}
+
+OrderItem.init(
+  {
+    id: {
+      type: UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    remark: {
+      type: TEXT({ length: "tiny" }),
+    },
+    qty: {
+      type: SMALLINT,
+      defaultValue: 1,
+    },
+  },
+  { sequelize }
+);
+
+module.exports = OrderItem;
