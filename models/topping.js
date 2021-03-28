c7b407692a864649165d63c09856206e71a58288
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-03-28 06:01:15 +0430

    Add Topping model

---
+++ models/topping.js
@@ -0,0 +1,26 @@
+const { Model, UUID, UUIDV4, STRING, SMALLINT } = require("sequelize");
+const sequelize = require("./sequelize");
+
+class Topping extends Model {}
+
+Topping.init(
+  {
+    id: {
+      type: UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    title: {
+      type: STRING(64),
+      allowNull: false,
+    },
+    price: {
+      type: SMALLINT,
+      allowNull: false,
+    },
+  },
+  { sequelize }
+);
+
+module.exports = Topping;
