961d7cc0445326528b1b5ce5425bc67ceb10703a
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-03-28 14:38:31 +0430

    Create "ProductCategory" model

---
+++ models/productCategory.js
@@ -0,0 +1,25 @@
+const { Model, UUID, UUIDV4, TEXT, STRING } = require("sequelize");
+const sequelize = require("./sequelize");
+
+class ProductCategory extends Model {}
+
+ProductCategory.init(
+  {
+    id: {
+      type: UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: STRING,
+      allowNull: false,
+    },
+    description: {
+      type: TEXT({ length: "tiny" }),
+    },
+  },
+  { sequelize }
+);
+
+module.exports = ProductCategory;
