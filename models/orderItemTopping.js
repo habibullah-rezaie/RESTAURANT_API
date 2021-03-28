06876867febe51cddfb02008586894e77b66bce0
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-03-28 06:02:36 +0430

    Add orderItemToppings, and productToppings model

---
+++ models/orderItemTopping.js
@@ -0,0 +1,18 @@
+const { Model, UUID, UUIDV4, TEXT, SMALLINT } = require("sequelize");
+const sequelize = require("./sequelize");
+
+class OrderItemTopping extends Model {}
+
+OrderItemTopping.init(
+  {
+    id: {
+      type: UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+  },
+  { sequelize }
+);
+
+module.exports = OrderItemTopping;
