d89d9cdae24ac5d7b94a8a76881344ac62fb66e0
nooruddin-rahmani <nooruddin.rahmani100@gmail.com>
2021-03-29 15:22:06 +0430

    handle add toppings request

--- controllers/admin/add-product.js
+++ controllers/admin/add-product.js
@@ -1,3 +1,4 @@
 exports.addProduct = async (req, res, next) => {};
 exports.addAllergrns = async (req, res, next) => {};
 exports.addAdditives = async (req, res, next) => {};
+exports.addToppings = async (req, res, next) => {};
--- routes/admin/add-product.js
+++ routes/admin/add-product.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { addProduct, addAllergrns, addAdditives } = require("../../controllers/admin/add-product");
+const { addProduct, addAllergrns, addAdditives, addToppings } = require("../../controllers/admin/add-product");
 const router = express.Router();
 
 // POST /admin/products/ -> add a product
 router.post("/", addProduct);
 router.post("/allergens", addAllergrns);
 router.post("/additives", addAdditives);
+router.post("/toppings", addToppings);
 
 module.exports = router;
