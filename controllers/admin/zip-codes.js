340a6859448152b6a53312559ba55d3277f163fc
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-03-29 22:25:31 +0430

    Define request handler functions

---
+++ controllers/admin/zip-codes.js
@@ -0,0 +1,8 @@
+// POST /admin/zipCodes/ => Add a new zipCode
+exports.addZipCode = async (req, res, next) => {}
+
+// PUT /admin/zipCodes/:id => Change zipCode
+exports.addZipCode = async (req, res, next) => {}
+
+// DELETE /admin/zipCodes/:id => Delete a zipCode
+exports.addZipCode = async (req, res, next) => {}
