81f8c40e3ee219530c720591b44ab26766c9b909
nooruddin-rahmani <nooruddin.rahmani100@gmail.com>
2021-03-29 15:57:55 +0430

    handle product category

--- controllers/admin/add-product.js
+++ controllers/admin/add-product.js
@@ -4,3 +4,4 @@ exports.addAdditives = async (req, res, next) => {};
 exports.addToppings = async (req, res, next) => {};
 exports.addFile = async (req, res, next) => {};
 exports.addProductCategory = async (req, res, next) => {};
+exports.deleteProductCategory = async (req, res, next) => {};
--- routes/admin/delete-product.js
+++ routes/admin/delete-product.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const { deleteProductCategory } = require("../../controllers/admin/add-product");
 const { deleteProduct, deleteProductImage, deleteProductAllergen} = require("../../controllers/admin/delete-product");
 const router = express.Router();
 router.delete("/:id", deleteProduct);
 router.delete("/:id", deleteProductImage)
 router.delete("/:id", deleteProductAllergen)
 router.delete("/:id", deleteProductAdditive)
+router.delete("/:id", deleteProductCategory)
 
 module.exports = router;
