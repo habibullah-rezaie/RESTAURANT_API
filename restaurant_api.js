dcf983493a302135796ce86279fa516e78fdadeb
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-03-29 14:18:35 +0430

    Allow Cross Origin requests

--- restaurant_api.js
+++ restaurant_api.js
@@ -6,9 +6,21 @@ const updateProductRoutes = require("./routes/admin/update-product");
 
 const app = express();
 
+// Allow CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // parse json requests
 app.use(json());
 
+// Use product updating related routes
 app.use("/admin/products", updateProductRoutes);
 
 sync(app);
