8a29b48f7c2c4e9f5c084035d8ae06a1a7d7508a
nooruddin-rahmani <nooruddin.rahmani100@gmail.com>
2021-03-29 15:47:53 +0430

    handle delete productimage and deleteproductallergen

--- controllers/admin/delete-product.js
+++ controllers/admin/delete-product.js
@@ -1,1 +1,3 @@
 exports.deleteProduct = async (req, res, next) => {};
+exports.deleteProductImage = async (req, res, next) => {};
+exports.deleteProductAllergen = async (req, res, next) => {};
--- routes/admin/delete-product.js
+++ routes/admin/delete-product.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const { deleteProduct } = require("../../controllers/admin/delete-product");
+const { deleteProduct, deleteProductImage, deleteProductAllergen} = require("../../controllers/admin/delete-product");
 const router = express.Router();
 router.delete("/:id", deleteProduct);
+router.delete("/:id", deleteProductImage)
+router.delete("/:id", deleteProductAllergen)
 
 module.exports = router;
