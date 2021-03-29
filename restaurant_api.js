00321e1ecb0ced850184c0b0caff05418444d8d0
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-03-29 14:07:42 +0430

    Parse json requests

--- restaurant_api.js
+++ restaurant_api.js
@@ -1,9 +1,13 @@
+const { json } = require("express");
 const express = require("express");
 
 const sync = require("./models/sync");
 const updateProductRoutes = require("./routes/admin/update-product");
 
 const app = express();
+
+// parse json requests
+app.use(json());
 
 app.use("/admin/products", updateProductRoutes);
 
