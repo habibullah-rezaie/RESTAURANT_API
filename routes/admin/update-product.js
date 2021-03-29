08fb08d2883edb01c41b662f3ef7524fb75f5e59
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-03-29 19:34:12 +0430

    Handle PUT /admin/products/allergens/:id updateProductAllergen

--- routes/admin/update-product.js
+++ routes/admin/update-product.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { updateProduct } = require("../../controllers/admin/update-product");
+const {
+  updateProduct,
+  updateProductAllergen,
+} = require("../../controllers/admin/update-product");
 const router = express.Router();
 
 // PUT /admin/products/:id -> update a product
 router.put("/:id", updateProduct);
+
+// PUT /admin/products/allergen/id -> change the text of a specifiec allergen
+router.put("/allergens/:id", updateProductAllergen);
 
 module.exports = router;
