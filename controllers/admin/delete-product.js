c6cecb32df82a3564aeba08f9034d42426b1f254
nooruddin-rahmani <nooruddin.rahmani100@gmail.com>
2021-03-29 15:50:43 +0430

    handle delete product additive request

--- controllers/admin/delete-product.js
+++ controllers/admin/delete-product.js
@@ -1,3 +1,4 @@
 exports.deleteProduct = async (req, res, next) => {};
 exports.deleteProductImage = async (req, res, next) => {};
 exports.deleteProductAllergen = async (req, res, next) => {};
+exports.deleteProductAdditive = async (req, res, next) => {};
--- routes/admin/delete-product.js
+++ routes/admin/delete-product.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.delete("/:id", deleteProduct);
 router.delete("/:id", deleteProductImage)
 router.delete("/:id", deleteProductAllergen)
+router.delete("/:id", deleteProductAdditive)
 
 module.exports = router;
