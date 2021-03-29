7c3931b9b25428209cbf2758d02a6c65fe421913
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-03-29 13:59:04 +0430

    Register routes in "update-products" file

--- restaurant_api.js
+++ restaurant_api.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
 const sync = require("./models/sync");
+const updateProductRoutes = require("./routes/admin/update-product");
 
 const app = express();
+
+app.use("/admin/products", updateProductRoutes);
 
 sync(app);
