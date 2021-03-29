fa3240f2efd6042709cce9dec39a990c3c031018
nooruddin-rahmani <nooruddin.rahmani100@gmail.com>
2021-03-29 15:23:56 +0430

    handle add file request

--- controllers/admin/add-product.js
+++ controllers/admin/add-product.js
@@ -2,3 +2,4 @@ exports.addProduct = async (req, res, next) => {};
 exports.addAllergrns = async (req, res, next) => {};
 exports.addAdditives = async (req, res, next) => {};
 exports.addToppings = async (req, res, next) => {};
+exports.addFile = async (req, res, next) => {};
--- routes/admin/add-product.js
+++ routes/admin/add-product.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addProduct, addAllergrns, addAdditives, addToppings } = require("../../controllers/admin/add-product");
+const { addProduct, addAllergrns, addAdditives, addToppings, addFile } = require("../../controllers/admin/add-product");
 const router = express.Router();
 
 // POST /admin/products/ -> add a product
@@ -7,5 +7,6 @@ router.post("/", addProduct);
 router.post("/allergens", addAllergrns);
 router.post("/additives", addAdditives);
 router.post("/toppings", addToppings);
+router.post("/files", addFile)
 
 module.exports = router;
