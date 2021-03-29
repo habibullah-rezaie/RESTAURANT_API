faf8a22c0d199303cbe06625922a6bbdcbbe4320
nooruddin-rahmani <nooruddin.rahmani100@gmail.com>
2021-03-29 16:22:45 +0430

    handle delete product topping

--- controllers/admin/add-product.js
+++ controllers/admin/add-product.js
@@ -5,3 +5,4 @@ exports.addToppings = async (req, res, next) => {};
 exports.addFile = async (req, res, next) => {};
 exports.addProductCategory = async (req, res, next) => {};
 exports.deleteProductCategory = async (req, res, next) => {};
+exports.deleteProductTopping = async (req, res, next) => {};
