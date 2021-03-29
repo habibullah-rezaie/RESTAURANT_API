58a69944fe463823e29d8cebd6066fb345ee3ab8
nooruddin-rahmani <nooruddin.rahmani100@gmail.com>
2021-03-29 21:02:58 +0430

    making middleware for add delete customer and view customer

--- restaurant_api.js
+++ restaurant_api.js
@@ -5,6 +5,8 @@ const express = require("express");
 const sync = require("./models/sync");
 const updateProductRoutes = require("./routes/admin/update-product");
 const addproductRoutes = require("./routes/admin/add-product")
+const deleteProductRoutes = require("./routes/admin/delete-product")
+const customer = require("./routes/admin/customer")
 
 const app = express();
 
@@ -28,5 +30,11 @@ app.use("/admin/products", updateProductRoutes);
 
 //used for product addition 
 app.use("/admin/products", addproductRoutes)
+
+//used for product deletion
+app.use("/admin/products", deleteProductRoutes)
+
+//used for customers view
+app.use("/admin/products", customer)
 
 sync(app);
