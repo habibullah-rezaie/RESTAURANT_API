8018ab4128c37179c3411add0b04b00e5da95b93
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-03-30 08:50:43 +0430

    Register "zipCode" routes

--- restaurant_api.js
+++ restaurant_api.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 const sync = require("./models/sync");
 const updateProductRoutes = require("./routes/admin/update-product");
+const zipCodeRoutes = require("./routes/admin/zipCodes");
 
 const app = express();
 
@@ -22,5 +23,8 @@ app.use(json());
 
 // Use product updating related routes
 app.use("/admin/products", updateProductRoutes);
+
+// Use routes related to zip code
+app.use("/admin/zipCodes", zipCodeRoutes);
 
 sync(app);
