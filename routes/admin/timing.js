93dd6426590b7649f58e033a64c50deb3e52b0e3
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-02 10:20:18 +0430

    Add validation

--- routes/admin/timing.js
+++ routes/admin/timing.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { body } = require("express-validator");
+const { body, param } = require("express-validator");
 const {
   addTiming,
   updateTiming,
@@ -65,7 +65,63 @@ router.post(
 
 // PUT /admin/timings/:day => update a timing on a
 // particular day
-router.put("/:day", updateTiming);
+router.put(
+  "/:day",
+  [
+    param("day")
+      .trim()
+      .toLowerCase()
+      .isIn([
+        "saturday",
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+      ])
+      .withMessage("Invalid day, not one of seven days in a week")
+      .custom(async (day, { req }) => {
+        const timing = await Timing.findByPk(day);
+
+        if (!timing)
+          throw new Error(
+            `Timing does not already exist for ${day} in order to update it.`
+          );
+
+        req.timing = timing;
+        return true;
+      }),
+    body("opening")
+      .trim()
+      .matches(
+        /^([2][0-3]|[01]?[0-9]):([1-5][0-9]|[0]?[1-9]]):([1-5][0-9]|[0]?[1-9])$/
+      )
+      .withMessage("Invalid opening time."),
+    body("closing")
+      .trim()
+      .matches(
+        /^([2][0-3]|[01]?[0-9]):([1-5][0-9]|[0]?[1-9]]):([1-5][0-9]|[0]?[1-9])$/
+      )
+      .withMessage("Invalid closing time.")
+      .custom((closing, { req }) => {
+        const opening = req.body.opening
+          .split(":")
+          .map((str) => Number.parseInt(str));
+
+        closing = closing.split(":").map((str) => Number.parseInt(str));
+
+        if (closing[0] < opening[0]) return false;
+        else if (closing[0] === opening[0])
+          if (opening[1] - closing[1] > -20) return false;
+        return true;
+      })
+      .withMessage(
+        "Closing time should not be less than opening time, and should at least differ 20 minutes."
+      ),
+  ],
+  updateTiming
+);
 
 // DELETE /admin/timings/:day => update a timing on
 // particular day
