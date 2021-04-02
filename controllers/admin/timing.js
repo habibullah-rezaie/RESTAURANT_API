617fca755132bd5edf439171811e712600ccbb09
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-02 09:52:38 +0430

    Implement "addTiming"

--- controllers/admin/timing.js
+++ controllers/admin/timing.js
@@ -1,10 +1,27 @@
-const { SendValidatorError } = require("../../utils/error");
+const { validationResult } = require("express-validator");
+const Timing = require("../../models/timing");
+const { sendValidatorError } = require("../../utils/error");
 
 // POST /admin/timings/ => add a timing
 exports.addTiming = async (req, res, next) => {
-  SendValidatorError(req, res);
+  // validation results
+  const errors = validationResult(req);
 
-  res.send('hi')
+  if (!errors.isEmpty()) return sendValidatorError(errors, res);
+
+  const { day, opening, closing } = req.body;
+
+  try {
+    const timing = await Timing.create({ day, opening, closing });
+    return res.status(201).json({
+      timing: timing,
+      message: "Successfully add timing for " + day,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+  new Date();
 };
 
 // PUT /admin/timings/:day => update a timing given day of the week
--- utils/error.js
+++ utils/error.js
@@ -1,14 +1,9 @@
-const { validationResult } = require("express-validator");
-
 /**
  *
- * @param {Express.Request} req A req that contains
+ * @param {[import("express-validator").ValidationError]} errors A req that contains
  * the errors throw by validator
  */
-exports.SendValidatorError = (req, res) => {
-  // validation results
-  const errors = validationResult(req);
-
+exports.sendValidatorError = (errors, res) => {
   // Validation errors has occured
   if (!errors.isEmpty()) {
     return res.status(422).json({
