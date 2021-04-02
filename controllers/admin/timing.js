461fb82ec4c77844951ddaaff0792d1206026428
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-02 04:37:29 +0430

    Structure timing controller

---
+++ controllers/admin/timing.js
@@ -0,0 +1,8 @@
+// POST /admin/timings/ => add a timing
+exports.addTiming = async (req, res, next) => {};
+
+// PUT /admin/timings/:day => update a timing given day of the week
+exports.updateTiming = async (req, res, next) => {};
+
+// DELETE /admin/timings/:day
+exports.deleteTiming = async (req, res, next) => {};
