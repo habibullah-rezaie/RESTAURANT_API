a8c59d977467924f0f4d3af9414c7f0f8a39e117
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-02 05:40:11 +0430

    Add validation to addTiming

--- controllers/admin/timing.js
+++ controllers/admin/timing.js
@@ -1,5 +1,11 @@
+const { SendValidatorError } = require("../../utils/error");
+
 // POST /admin/timings/ => add a timing
-exports.addTiming = async (req, res, next) => {};
+exports.addTiming = async (req, res, next) => {
+  SendValidatorError(req, res);
+
+  res.send('hi')
+};
 
 // PUT /admin/timings/:day => update a timing given day of the week
 exports.updateTiming = async (req, res, next) => {};
--- routes/admin/timing.js
+++ routes/admin/timing.js
@@ -1,14 +1,52 @@
 const express = require("express");
+const { body } = require("express-validator");
 const {
   addTiming,
   updateTiming,
   deleteTiming,
 } = require("../../controllers/admin/timing");
+const Timing = require("../../models/timing");
 
 const router = express.Router();
 
 // POST /admin/timings/ => add a timing
-router.post("/", addTiming);
+router.post(
+  "/",
+  [
+    body("day")
+      .trim()
+      .toLowerCase()
+      .isIn([
+        "saturday",
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+      ])
+      .withMessage("Invalid day, not one of seven days in a week")
+      .custom(async (day, {}) => {
+        const timing = await Timing.findByPk(day);
+
+        if (timing) throw new Error(`A timing already exists in ${day}`);
+        return true;
+      }),
+    body("opening")
+      .trim()
+      .matches(
+        /^([2][0-3]|[01]?[0-9]):([0-5][1-9]|[0]?[1-9]]):([0-5][1-9]|[0]?[1-9])$/
+      )
+      .withMessage("Invalid opening time."),
+    body("closing")
+      .trim()
+      .matches(
+        /^([2][0-3]|[01]?[0-9]):([0-5][1-9]|[0]?[1-9]]):([0-5][1-9]|[0]?[1-9])$/
+      )
+      .withMessage("Invalid closing time."),
+  ],
+  addTiming
+);
 
 // PUT /admin/timings/:day => update a timing on a
 // particular day
