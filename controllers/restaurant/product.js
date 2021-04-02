1adef84619f9eabceec0550fea4684c9ff4444bf
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-02 14:48:34 +0430

    Structure product related client related request handlers

---
+++ controllers/restaurant/product.js
@@ -0,0 +1,17 @@
+// GET / products/ => Get list of products
+exports.getProducts = async (req, res, next) => {};
+
+// GET / products/:id => Get detail of one product
+exports.getProduct = async (req, res, next) => {};
+
+// GET / products/toppings => Get get toppings of a single product
+exports.getToppings = async (req, res, next) => {};
+
+// GET / products/allergens => Get Allergens of a single product
+exports.getAllergens = async (req, res, next) => {};
+
+// GET / products/additives => Get Additives of a single product
+exports.getAdditives = async (req, res, next) => {};
+
+// GET / products/files => Get Files of a single product
+exports.getFiles = async (req, res, next) => {};
