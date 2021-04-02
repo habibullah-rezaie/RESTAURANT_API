1380d406eaa7a0b03b1f3c7d1825d5e66907cfe0
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-02 12:32:26 +0430

    Add validation for order query params

--- routes/admin/customer.js
+++ routes/admin/customer.js
@@ -1,10 +1,22 @@
 const express = require("express");
-const { getCustomers } = require("../../controllers/admin/customer");
+const { query } = require("express-validator");
+
+const {
+  getCustomers,
+  getCustomer,
+} = require("../../controllers/admin/customer");
 
 const router = express.Router();
 
 // GET /admin/customers/ => Show customers
-router.get("/", getCustomers);
+router.get(
+  "/",
+  query("order")
+    .trim()
+    .isIn(["ASC", "DESC"])
+    .withMessage('The query order should be either "ASC" or "DESC".'),
+  getCustomers
+);
 
 // GET /admin/customers/:id => get info about a customer
 router.get("/:id", getCustomer);
