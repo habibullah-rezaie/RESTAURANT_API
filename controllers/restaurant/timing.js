b9c06538d15c66dfb45a3076a6071f9afba11054
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-03 11:13:50 +0430

    Implement getTimings controller function

--- controllers/restaurant/timing.js
+++ controllers/restaurant/timing.js
@@ -1,1 +1,20 @@
-exports.getTimings = async (req, res, next) => {};
+const Timing = require("../../models/timing");
+
+exports.getTimings = async (req, res, next) => {
+  try {
+    const timings = await Timing.findAll();
+    if (!timings) {
+      const error = new Error("Failed to fetch timings");
+      error.statusCode = 500;
+      throw error;
+    }
+
+    res.status(200).json({
+      timings: timings,
+      message: "Successfully fetched all timings",
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
