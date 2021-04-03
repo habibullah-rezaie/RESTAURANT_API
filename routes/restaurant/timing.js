4acca0b61de814d2ef6be44e4caa855f9cc5a498
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-03 11:14:46 +0430

    Fix route matching problem

--- routes/restaurant/timing.js
+++ routes/restaurant/timing.js
@@ -4,6 +4,6 @@ const { getTimings } = require("../../controllers/restaurant/timing");
 const router = express.Router();
 
 // GET /timings => get list of timings
-router.get("/timings", getTimings);
+router.get("/", getTimings);
 
 module.exports = router;
