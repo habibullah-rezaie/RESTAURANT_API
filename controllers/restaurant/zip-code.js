3003be19343ac2207b79edf047b93c4aa520251a
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-03 11:30:50 +0430

    Create "getZipCodes" controller function

---
+++ controllers/restaurant/zip-code.js
@@ -0,0 +1,20 @@
+const ZipCode = require("../../models/zipCode");
+
+exports.getZipCodes = async (req, res, next) => {
+  try {
+    const zipCodes = await ZipCode.findAll();
+    if (!zipCodes) {
+      const error = new Error("Failed to fetch zip codes");
+      error.statusCode = 500;
+      throw error;
+    }
+
+    res.status(200).json({
+      zipCodes: zipCodes,
+      message: "Successfully fetched all zip codes",
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
