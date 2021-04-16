9b95ea33bc3619a292a60449d43a38da1efad75e
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-16 15:23:15 +0430

    Delete unnecessary route files

--- routes/admin/dalete-timing.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const timingControllers = require("../../controllers/admin/delete-timing");
-router.delete("/:day", timingControllers.deleteTiming);
-
-module.exports = router;
