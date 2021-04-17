32224033b2911d84640c897b8c72b39cffdf0e6b
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-17 21:01:42 +0430

    Add validation middlewares for DELETE /admin/products/:productId/files/:file

--- routes/admin/delete-product.js
+++ routes/admin/delete-product.js
@@ -31,7 +31,36 @@ router.delete(
   deleteProduct
 );
 
-router.delete("/:id", deleteProductFile);
+// Handle deleting a special file of a special product.
+router.delete(
+  "/:productId/files/:file",
+  [
+    param("productId")
+      .trim()
+      .isUUID(4)
+      .withMessage("Invalid id format")
+      .custom(async (id, { req }) => {
+        const product = await Product.findByPk(id);
+        if (!product) throw new Error("No product exist with given id");
+
+        req.product = product;
+      }),
+    param("file")
+      .trim()
+      .custom(async (fileName, { req }) => {
+        if (!req.product) return; // Abort becuase no product found.
+        const [file] = await req.product.getFiles({ where: { fileName } });
+
+        if (!file)
+          throw new Error(
+            "No file related to current product found with given name."
+          );
+
+        req.file = file;
+      }),
+  ],
+  deleteProductFile
+);
 router.delete("/allergens/:id", deleteProductAllergen);
 router.delete("/additives/:id", deleteProductAdditive);
 router.delete("/categories/:id", deleteProductCategory);
