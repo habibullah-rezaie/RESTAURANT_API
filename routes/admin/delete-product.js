b1aed4ef021a08696e2cb25f72f8687956d35830
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-17 19:25:34 +0430

    Add validation middlewares for DELETE /admin/products/:id

--- routes/admin/delete-product.js
+++ routes/admin/delete-product.js
@@ -1,17 +1,37 @@
 const express = require("express");
+const { param } = require("express-validator");
+
 const {
   deleteProductCategory,
 } = require("../../controllers/admin/delete-product");
 const {
   deleteProduct,
-  deleteProductImage,
+  deleteProductFile,
   deleteProductAllergen,
   deleteProductAdditive,
   deleteProductTopping,
 } = require("../../controllers/admin/delete-product");
+const Product = require("../../models/product");
+
 const router = express.Router();
-router.delete("/:id", deleteProduct);
-router.delete("/:id", deleteProductImage);
+
+// DELETE /admin/products/:id => delete a product
+router.delete(
+  "/:id",
+  param("id")
+    .trim()
+    .isUUID(4)
+    .withMessage("Invalid id format")
+    .custom(async (id, { req }) => {
+      const product = await Product.findByPk(id);
+      if (!product) throw new Error("No product exist with given id");
+
+      req.product = product;
+    }),
+  deleteProduct
+);
+
+router.delete("/:id", deleteProductFile);
 router.delete("/allergens/:id", deleteProductAllergen);
 router.delete("/additives/:id", deleteProductAdditive);
 router.delete("/categories/:id", deleteProductCategory);
