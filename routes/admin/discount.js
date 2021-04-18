312053f82c2d7245d5a3a2b2d79888103d9f6871
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-18 13:51:06 +0430

    Add validation

--- routes/admin/discount.js
+++ routes/admin/discount.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const { body } = require("express-validator");
 const { discountAll } = require("../../controllers/admin/discount");
 const { isAuthenticated } = require("../../utils/auth");
 
 const router = express.Router();
 
 // PATCH /discountAll => give discount to all products
-router.patch("/discountAll", isAuthenticated, [], discountAll);
+router.patch(
+  "/discountAll",
+  isAuthenticated,
+  [
+    body("discount")
+      .trim()
+      .isNumeric()
+      .withMessage("Discount should be numeric")
+      .toFloat()
+      .custom((discount) => {
+        if (!(0 <= discount && discount <= 100))
+          throw new Error("Discount should be between 1 - 100.");
+        return true;
+      }),
+  ],
+  discountAll
+);
 
 module.exports = router;
