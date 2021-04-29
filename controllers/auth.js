bf3f2278ed2ec0834204a0ec7557969875330b5f
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-29 09:02:57 +0430

    Move login  controller new file

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
+const { generateAccessToken } = require("../../utils/auth");
 
 const Admin = require("../../models/admin");
 const RefreshToken = require("../../models/refreshToken");
@@ -51,4 +52,35 @@ exports.login = async (req, res, next) => {
     console.error(err);
     next(err);
   }
+};
+
+const getToken = async (req, res, next) => {
+  // validation results
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return sendValidatorError(errors, res);
+
+  const { refreshToken } = req;
+
+  try {
+    jwt.verify(
+      refreshToken.token,
+      process.env.JWT_REFRESH_TOKEN_SECRET,
+      (err, data) => {
+        if (err) throwError(err.message, 403);
+
+        const token = generateAccessToken(data);
+
+        res.status(200).json({
+          token,
+          message: "Access token generated.",
+        });
+      }
+    );
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getToken,
 };
