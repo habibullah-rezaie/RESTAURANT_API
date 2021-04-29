6ebe338898fb53a686ee3d24b1396cac9372222e
habibullah-rezaie <habibullah.rezaie.8@gmail.com>
2021-04-29 09:40:33 +0430

    Add GET 'auth/token' route

--- routes/auth.js
+++ routes/auth.js
@@ -44,4 +44,47 @@ router.post(
   login
 );
 
+const { getToken } = require("../controllers/auth");
+const RefreshToken = require("../models/refreshToken");
+
+router.get(
+  "/token",
+  [
+    body().custom((reqBody) => {
+      console.log(reqBody);
+      return true;
+    }),
+    body("refreshToken")
+      .isString()
+      .withMessage({
+        msg: "Refresh token should be a String.",
+        specialType: "AUTHENTICATION_FAILURE",
+      })
+      .isLength({ min: 10 })
+      .withMessage({
+        msg: "Refresh token has invalid length.",
+        specialType: "AUTHENTICATION_FAILURE",
+      })
+      .custom(async (token, { req }) => {
+        let fetchedToken = null;
+        try {
+          fetchedToken = await RefreshToken.findByPk(token);
+        } catch (err) {
+          throw {
+            specialType: "SuquelizeError",
+            message: "Something went wrong, trying to fix it.",
+            error: err,
+          };
+        }
+
+        if (!fetchedToken) {
+          throw new Error("Refresh token does not exist.");
+        }
+
+        req.refreshToken = fetchedToken;
+      }),
+  ],
+  getToken
+);
+
 module.exports = router;
